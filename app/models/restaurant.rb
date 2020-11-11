99bf96fe6404df369f5c5f9db92cb7e00ecd2daf
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-11 15:55:47 +0100

    Tables validation WIP : remains 2 errors to correct

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
